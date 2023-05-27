ea040d94fa6d59efae4625b2c970c0f8fde46e21
Athvaith <athvaith.k@gmail.com>
2023-05-27 23:06:10 +0530

    Add Config related to train.py

--- config/train_config.py
+++ config/train_config.py
@@ -1,5 +1,16 @@
 import os
 
+import config as cg
+
 batch_size = 512
 
 n_worker = int(os.cpu_count())
+
+model_path = os.path.join(cg.data_path, 'model')
+
+isExist = os.path.exists(model_path)
+
+if not isExist:
+    os.makedirs(model_path)
+
+model_path = os.path.join(model_path,'best.pt')
