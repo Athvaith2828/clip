7a9994883e6d7a5dd5f77c3b7c7511dd6fba2ab7
Athvaith <athvaith.k@gmail.com>
2023-05-27 23:06:35 +0530

    Add model config

---
+++ config/model_config.py
@@ -0,0 +1,22 @@
+import os.path
+
+import config as cg
+
+image_model = 'vit_base_patch16_224'
+
+image_dimension = 768
+
+text_model = 'distilbert-base-uncased'
+
+text_dimension = 768
+
+pretrained = True
+
+trainable = True
+
+projection_dim = 256
+
+dropout = 0.1
+
+temperature = 1.0
+
